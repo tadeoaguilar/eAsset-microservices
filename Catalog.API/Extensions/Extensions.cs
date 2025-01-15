a6167284baf9600aeeac35682542bc5484587e94
tadeoaguilar <tadeo.am@gmail.com>
2025-01-14 21:49:17 -0500

    Tenants Seeding

--- Catalog.API/Extensions/Extensions.cs
+++ Catalog.API/Extensions/Extensions.cs
@@ -18,10 +18,19 @@ namespace Catalog.API.Extensions
                     var licenseJsonPath = Path.Combine(AppContext.BaseDirectory, "Setup", "licenses.json");
                     var licenseData = File.ReadAllText(licenseJsonPath);
                     var licenses = JsonSerializer.Deserialize<List<License>>(licenseData);
-
+                    var tenantJsonPath = Path.Combine(AppContext.BaseDirectory, "Setup", "tenants.json");
+                    var tenantData = File.ReadAllText(tenantJsonPath);
+                    var tenants = JsonSerializer.Deserialize<List<Tenant>>(tenantData);
+                   
                     if (licenses != null)
                     {
+                       
                         context.Set<License>().AddRange(licenses);
+                        context.SaveChanges();
+                    }
+                    if (tenants != null)
+                    {
+                        context.Set<Tenant>().AddRange(tenants);
                         context.SaveChanges();
                     }
 
